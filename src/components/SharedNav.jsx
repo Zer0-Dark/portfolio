71b1d03d4b66df382f494dfd30d77631390d8d2b
Zer0-Dark <60540945+Zer0-Dark@users.noreply.github.com>
2023-09-17 01:03:24 +0300

    adding the nav bar for the mobile

--- src/components/SharedNav.jsx
+++ src/components/SharedNav.jsx
@@ -1,25 +1,60 @@
 import { NavLink, Outlet } from "react-router-dom";
+import info from "../photos/more.png";
+import { useEffect, useState } from "react";
+function SharedNavBar() {
+    let [mobile, setMobile] = useState("nav-bar-items-mobile");
+    const [windowSize, setWindowSize] = useState(getWindowSize());
 
-function SharedNavBar(){
+    useEffect(() => {
+        function handleWindowResize() {
+            setWindowSize(getWindowSize());
+        }
 
-    return(
+        window.addEventListener("resize", handleWindowResize);
+
+        return () => {
+            window.removeEventListener("resize", handleWindowResize);
+        };
+    }, []);
+    function getWindowSize() {
+        const { innerWidth, innerHeight } = window;
+        return { innerWidth, innerHeight };
+    }
+
+
+    function settingMobile() {
+        if (mobile === "nav-bar-items-mobile") {
+            setMobile("nav-bar-items");
+        } else {
+            setMobile("nav-bar-items-mobile");
+        }
+    }
+    useEffect(() => {
+        if (windowSize.innerWidth > 900) {
+            setMobile("nav-bar-items");
+        }
+    }, [windowSize.innerWidth]);
+
+    return (
         <>
-        <div className="nav-bar">
-            <div className="nav-bar-logo">
-            <h3>ZERO</h3>
+            <div className="nav-bar">
+                <div className="nav-bar-logo">
+                    <h3>ZERO</h3>
+                </div>
+                <div className={mobile}>
+                    <NavLink to={"/"}>Home</NavLink>
+                    <NavLink to={"about"}>Who Am I ?</NavLink>
+                    <NavLink to={"projects"}>Projects</NavLink>
+                    <NavLink to={"contact"}>{"Contact <Me"}</NavLink>
+                </div>
+                <button onClick={settingMobile} className="more-info">
+                    <img src={info}></img>
+                </button>
             </div>
-            <div className="nav-bar-items">
-            <NavLink to={"/"}>Home</NavLink>
-            <NavLink to={"about"}>Who Am I ?</NavLink>
-            <NavLink to={"projects"}>Projects</NavLink>
-            <NavLink to={"contact"}>{"Contact <Me"}</NavLink>
-            </div>
-        </div>
-    
-        <Outlet></Outlet>
 
+            <Outlet></Outlet>
         </>
-    )
+    );
 }
 
 export default SharedNavBar;
