794d1aa5cafe8521d7f1c76e28f0a6fe2c70dde3
Zer0-Dark <60540945+Zer0-Dark@users.noreply.github.com>
2023-09-18 04:03:13 +0300

    fixing the navbar issue

--- src/components/SharedNav.jsx
+++ src/components/SharedNav.jsx
@@ -1,39 +1,18 @@
 import { NavLink, Outlet } from "react-router-dom";
 import info from "../photos/more.png";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 function SharedNavBar() {
-    let [mobile, setMobile] = useState("nav-bar-items-mobile");
-    const [windowSize, setWindowSize] = useState(getWindowSize());
-
-    useEffect(() => {
-        function handleWindowResize() {
-            setWindowSize(getWindowSize());
-        }
-
-        window.addEventListener("resize", handleWindowResize);
-
-        return () => {
-            window.removeEventListener("resize", handleWindowResize);
-        };
-    }, []);
-    function getWindowSize() {
-        const { innerWidth, innerHeight } = window;
-        return { innerWidth, innerHeight };
-    }
+    let [mobile, setMobile] = useState("nav-bar-items");
 
 
     function settingMobile() {
-        if (mobile === "nav-bar-items-mobile") {
+        if (mobile === "nav-bar-items-active") {
             setMobile("nav-bar-items");
         } else {
-            setMobile("nav-bar-items-mobile");
+            setMobile("nav-bar-items-active");
         }
     }
-    useEffect(() => {
-        if (windowSize.innerWidth > 900) {
-            setMobile("nav-bar-items");
-        }
-    }, [windowSize.innerWidth]);
+
 
     return (
         <>
@@ -42,12 +21,12 @@ function SharedNavBar() {
                     <h3>ZERO</h3>
                 </div>
                 <div className={mobile}>
-                    <NavLink to={"/"}>Home</NavLink>
-                    <NavLink to={"about"}>Who Am I ?</NavLink>
-                    <NavLink to={"projects"}>Projects</NavLink>
-                    <NavLink to={"contact"}>{"Contact <Me"}</NavLink>
+                    <NavLink onClick={settingMobile} to={"/"}>Home</NavLink>
+                    <NavLink onClick={settingMobile} to={"about"}>Who Am I ?</NavLink>
+                    <NavLink onClick={settingMobile} to={"projects"}>Projects</NavLink>
+                    <NavLink onClick={settingMobile} to={"contact"}>{"Contact <Me"}</NavLink>
                 </div>
-                <button onClick={settingMobile} className="more-info">
+                <button className="more-info" onClick={settingMobile}>
                     <img src={info}></img>
                 </button>
             </div>
