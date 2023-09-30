103bbb28f792228ff1d1aa2d497367b50458c4ca
Zer0-Dark <60540945+Zer0-Dark@users.noreply.github.com>
2023-10-01 00:03:08 +0300

    fixing some issues

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,19 +3,25 @@ import Github from "../photos/github.svg"
 import eyes from "../photos/eye-solid.svg"
 import { motion } from "framer-motion"
 import testPhoto from "../photos/nier.png"
+import { useState } from "react"
 export default function ProjectCard(props) {
+    let [test, setTest] = useState(0);
+    function testing() {
+        test === 0 ? setTest(1) : setTest(0)
+    }
     return (
         <motion.div className="card"
+            onClick={testing}
             initial={{ opacity: 0.7, y: 10 }}
             animate={{ opacity: 1, y: 0 }}
             transition={{ duration: 0.5 }}
             whileHover={{ scale: 1.01, y: -20 }}
         >
             <div className="card-img">
-                <img src={props.img}></img>
+                <img src={testPhoto}></img>
             </div>
             <motion.div
-                initial={{ opacity: 0 }}
+                initial={{ opacity: test }}
                 whileHover={{ opacity: 0.9 }}
                 className="card-info">
                 <h1>{props.name}</h1>
